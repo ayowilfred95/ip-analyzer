4a8a093c6b43935a39d999c34b859f9399a78594
ayowilfred95 <38457345+ayowilfred95@users.noreply.github.com>
2024-04-09 16:05:36 +0100

    integrated live api's into the frontend

--- sextant/src/PylonConnector.js
+++ sextant/src/PylonConnector.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { w3cwebsocket as W3CWebSocket } from "websocket";
-const client = new W3CWebSocket('ws://localhost:55455');
+const client = new W3CWebSocket('wss://ip-analyzer-2-0-0.onrender.com');
 
 class PylonConnector extends Component {
     constructor(props) {
